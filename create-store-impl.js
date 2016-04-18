d33f093a4650e5ca09e6ff223d6e557aa458c1e0
Vasa <vasanth.krishnamoorthy@citrix.com>
2016-04-18 16:12:08 -0700

    Create store implementation

--- create-store-impl.js
+++ create-store-impl.js
@@ -1,5 +1,3 @@
-// Let's stry to implement the createStore method of Redux to understand it better
-
 const counter = (state = 0, action) => {
   switch (action.type) {
     case 'INCREMENT':
@@ -9,42 +7,41 @@ const counter = (state = 0, action) => {
     default:
       return state;
   }
-};
+}
 
-const createStore = (reducer) => {
-  let state;
-  let listeners = [];
+const Counter = ({
+  value,
+  onIncrement,
+  onDecrement
+  }) => (
+  <div>
+    <h1>{value}</h1>
+    <button onClick={onIncrement}>+</button>
+    <button onClick={onDecrement}>-</button>
+  </div>
+);
 
-  const getState = () => state;
-
-  const dispatch = (action) => {
-    state = reducer(state, action);
-    listeners.forEach(listener => listener());
-  };
-
-  const subscribe = (listener) => {
-    listeners.push(listener);
-
-    // We return a fn -- which can be used to unsubscribe the current listener.
-    return () => {
-      listeners = listeners.filter(l => l !== listener);
-    };
-  };
-
-  dispatch({});
-
-  return {getState, dispatch, subscribe};
-};
-
+const { createStore } = Redux;
 const store = createStore(counter);
 
 const render = () => {
-  document.body.innerText = store.getState();
+  ReactDOM.render(
+    <Counter
+      value={store.getState()}
+      onIncrement={() =>
+        store.dispatch({
+          type: 'INCREMENT'           
+        })            
+      }
+      onDecrement={() =>
+        store.dispatch({
+          type: 'DECREMENT'           
+        })            
+      }
+    />,
+    document.getElementById('root')
+  );
 };
 
 store.subscribe(render);
 render();
-
-document.addEventListener('click', () => {
-  store.dispatch({type: 'INCREMENT'});
-});
