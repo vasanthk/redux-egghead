206b6eb753a201390031b6038e1e8caa7abcb786
vasanthk <vasa.httpster@gmail.com>
2016-06-04 16:39:53 -0700

    Added all actions for Todo app

--- idiomatic-redux/1. arrow-fn.js
+++
@@ -1,6 +0,0 @@
-// Just return the action object by wrapping it around parens in an arrow function.
-export const addTodo = (text) => ({
-  type: 'ADD_TODO',
-  id: (nextTodoId++).toString(),
-  text
-});
